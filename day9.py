a295bc0973a2d30cf3e7a4deb527d0a223604ba5
Vijay Patel <spiritedtechie@gmail.com>
2024-12-09 21:42:28 +0000

    day9 - cleanup

--- day9.py
+++ day9.py
@@ -1,123 +1,100 @@
-from collections import defaultdict
-import re
-
 def read_file(filename):
     with open(filename) as file:
         return list(file.readline().strip())
 
 
 def explode_input(line):
-    # explode original line
-    newlist = []
-    for ind, val in enumerate(line):
-        if ind % 2 == 0:
-            newlist.extend([ind // 2] * int(val))
-        else:
-            new_entries = ["."] * int(val)
-            if new_entries:
-                newlist.extend(new_entries)
-
-    return newlist
+    return [
+        i // 2 if i % 2 == 0 else "."
+        for i, char in enumerate(line)
+        for _ in range(int(char))  # Repeat character based on its count
+    ]
 
 
-def calculate_checksum(list):
-    checksum = 0
-    for ind, val in enumerate(list):
-        if val != ".":
-            # checksum remainder
-            checksum += ind * val
-    return checksum
+def calculate_checksum(line):
+    return sum(ind * val for ind, val in enumerate(line) if val != ".")
 
-# O(n)
-def part_1(line: list):
-    newlist = explode_input(line)
 
-    # use two pointers to swap relevant positions and calculate checksum on the way
-    left_ptr = 0
-    right_ptr = len(newlist) - 1
+def find_contiguous_blocks(line, freespace=True):
+    matcher = lambda chr: chr != "." if freespace else chr == "."
+    blocks = []
+    i = 0
+    while i < len(line):
+        # get to start of contiguous data
+        if matcher(line[i]):
+            i += 1
+            continue
 
-    while left_ptr < right_ptr:
-        if newlist[left_ptr] == "." and newlist[right_ptr] != ".":
-            # swap
-            tmp = newlist[left_ptr]
-            newlist[left_ptr] = newlist[right_ptr]
-            newlist[right_ptr] = tmp
+        # find end of next contiguous data
+        j = i
+        while j + 1 < len(line) and line[j + 1] == line[i]:
+            j += 1
 
-        # shift to next positions
-        while newlist[left_ptr] != ".":
-            left_ptr += 1
-        while newlist[right_ptr] == ".":
-            right_ptr -= 1
+        # store the contiguous data range
+        blocks.append((i, j))
 
-    # calculate checksum
-    checksum = calculate_checksum(newlist)
+        # repeat
+        i = j + 1
 
-    print(checksum)
+    return blocks
+
+
+def move_blocks_to_free_space(line: list, file_blocks: list, free_space_blocks: list):
+    # process blocks in reverse to move into first possible free space
+    for block_start, block_end in file_blocks[::-1]:
+        block_size = block_end - block_start + 1
+
+        for idx, (free_start, free_end) in enumerate(free_space_blocks):
+            free_space_size = free_end - free_start + 1
+            if free_start < block_start and free_space_size >= block_size:
+                # move block into free space
+                line[free_start : free_start + block_size] = line[block_start : block_end + 1]
+                line[block_start : block_end + 1] = ["."] * (block_size)
+
+                # update free space now used
+                if free_end == free_start + block_size -1:
+                    free_space_blocks.remove((free_start, free_end))
+                else:
+                    free_space_blocks[idx] = (free_start + block_size, free_end)
+
+                break
 
 
 def part_2(line: list):
     line = explode_input(line)
 
-    def find_contiguous_blocks(line, freespace=True):
-        matcher = lambda chr : chr != '.' if freespace else chr == '.'
-        blocks = list()
-        i = 0
-        while i < len(line):
-            # get to start of contiguous data
-            if matcher(line[i]):
-                i += 1
-                continue
-                
-            # find end of next contiguous data
-            j = i
-            while j + 1 < len(line) and line[j + 1] == line[i]:
-                j += 1
+    file_blocks = find_contiguous_blocks(line, freespace=False)
+    free_space_blocks = find_contiguous_blocks(line)
 
-            # store the contiguous data range
-            blocks.append((i, j))
+    move_blocks_to_free_space(line, file_blocks, free_space_blocks)
 
-            # repeat
-            i = j + 1
-            
-        return blocks
-
-
-
-    blocks_to_move = find_contiguous_blocks(line, freespace=False)
-    free_space = find_contiguous_blocks(line)
-    
-
-    # process blocks to move into first possible free space that can take it
-    for bi, bj in blocks_to_move[::-1]:
-
-        block_size = bj - bi
-
-        for idx, (fi, fj) in enumerate(free_space):
-            if fi < bi and fj-fi >= block_size:
-                # swap
-                n2 = (['.'] * int(bj-bi+1))
-                n1 =  line[bi: bj + 1]
-
-                line[fi: fi+block_size+1] = n1
-                line[bi: bj+1] = n2
-
-                # update free space now used
-                if fj == fi+block_size:
-                    free_space.remove((fi, fj))
-                else:
-                    free_space[idx] = (fi+block_size+1, fj)
-
-                break
-
-                
-
-    # calculate checksum
     checksum = calculate_checksum(line)
-    print(checksum) 
+    print(checksum)
+
+
+# O(n)
+def part_1(line: list):
+    def swap_elements(line: list, left_idx: int, right_idx: int):
+        line[left_idx], line[right_idx] = line[right_idx], line[left_idx]
+
+    line = explode_input(line)
+
+    # use two pointers to swap relevant positions and calculate checksum on the way
+    left_ptr, right_ptr = 0, len(line) - 1
+    while left_ptr < right_ptr:
+        if line[left_ptr] == "." and line[right_ptr] != ".":
+            swap_elements(line, left_ptr, right_ptr)
+
+        # shift to next positions
+        left_ptr += 1 if line[left_ptr] != "." else 0
+        right_ptr -= 1 if line[right_ptr] == "." else 0
+
+    checksum = calculate_checksum(line)
+    print(checksum)
 
 
 if __name__ == "__main__":
     line = read_file("day9.txt")
 
-    # part_1(line)
+    part_1(line)
     part_2(line)
