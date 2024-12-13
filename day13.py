f57df51776f8e763c15c46007910a1fae9fdcc66
Vijay Patel <spiritedtechie@gmail.com>
2024-12-13 21:53:28 +0000

    day 13 - part 2

--- day13.py
+++ day13.py
@@ -9,7 +9,7 @@ pattern = re.compile(
 
 # return list of tuples ((94, 34), (22, 67), (8400, 5400))
 # in form ((button A x and y increments), (button B x and y increments), (target x and y))
-def parse_file(filename):
+def parse_file(filename, target_add=0):
     with open(filename, "r") as file:
         data = file.read()
 
@@ -19,35 +19,49 @@ def parse_file(filename):
     for match in matches:
         button_a = (int(match[0]), int(match[1]))
         button_b = (int(match[2]), int(match[3]))
-        prize = (int(match[4]), int(match[5]))
+        prize = (int(match[4]) + target_add, int(match[5]) + target_add)
         parsed_data.append((button_a, button_b, prize))
 
     return parsed_data
 
 
 def calculate_for_machine(machine_data):
-    target_x, target_y = machine_data[2]
-    btn_a_x_inc, btn_a_y_inc = machine_data[0]
-    btn_b_x_inc, btn_b_y_inc = machine_data[1]
+    (Xa, Ya), (Xb, Yb), (Xp, Yp) = machine_data
 
     for a in range(1, 101):
-        xi = a * btn_a_x_inc
-        yi = a * btn_a_y_inc
-
         for b in range(1, 101):
-            xj = b * btn_b_x_inc
-            yj = b * btn_b_y_inc
-
-            if xi + xj == target_x and yi + yj == target_y:
+            if Xa * a + Xb * b == Xp and Ya * a + Yb * b == Yp:
                 return a, b
 
-    return (0, 0)
+    return 0, 0
+
+
+def fast_calculate_for_machine(machine_data):
+    from sympy import symbols, Eq, solve
+
+    (Xa, Ya), (Xb, Yb), (Xp, Yp) = machine_data
+
+    a, b = symbols("a,b")
+
+    # simultaneous equations to solve
+    eq1 = Eq(Xa * a + Xb * b, Xp)
+    eq2 = Eq(Ya * a + Yb * b, Yp)
+
+    solutions = solve([eq1, eq2], (a, b))
+
+    if all(solutions[var].is_integer for var in [a, b]):
+        return solutions[a], solutions[b]
+
+    return 0, 0
+
+
+def calculate(machines, alg_func):
+    return sum(3 * a + b for machine in machines for a, b in [alg_func(machine)])
 
 
 # Main
 machines = parse_file("day13.txt")
+machines_complex = parse_file("day13.txt", target_add=10000000000000)
 
-cost = sum(
-    3 * a + b for machine in machines for a, b in [calculate_for_machine(machine)]
-)
-print(cost)
+print("part_1:", calculate(machines, calculate_for_machine))
+print("part_2:", calculate(machines_complex, fast_calculate_for_machine))
