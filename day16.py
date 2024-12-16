898fee929869c5fda3d67b7ec3001e9c29496cf2
Vijay Patel <spiritedtechie@gmail.com>
2024-12-16 22:29:30 +0000

    day 16 - part 2

--- day16.py
+++ day16.py
@@ -1,5 +1,5 @@
 import heapq
-
+from collections import defaultdict
 
 DIRECTIONS = {"^": (-1, 0), ">": (0, 1), "V": (1, 0), "<": (0, -1)}
 
@@ -12,60 +12,67 @@ def read_file(filename):
 def get_neighbours(grid, r, c, direction):
     neighbours = []
 
-    # same direction
-    dr, dc = DIRECTIONS[direction]
-    nr, nc = r + dr, c + dc
-    if grid[nr][nc] != "#":
-        neighbours.append((nr, nc, direction, 1))
-
-    # rotations (clockwise and counter)
     dir_list = list(DIRECTIONS)
-    new_dir = dir_list[(dir_list.index(direction) + 1) % 4]
-    neighbours.append((r, c, new_dir, 1000))
-    new_dir = dir_list[(dir_list.index(direction) - 1) % 4]
-    neighbours.append((r, c, new_dir, 1000))
+    same_dir = direction
+    clockwise_dir = dir_list[(dir_list.index(direction) + 1) % 4]
+    anticlockwise_dir = dir_list[(dir_list.index(direction) - 1) % 4]
+
+    to_process = [(same_dir, 1), (clockwise_dir, 1001), (anticlockwise_dir, 1001)]
+
+    for dir, cost in to_process:
+        dr, dc = DIRECTIONS[dir]
+        nr, nc = r + dr, c + dc
+        if grid[nr][nc] != "#":
+            neighbours.append((nr, nc, dir, cost))
 
     return neighbours
 
+
 # The priority queue is key to ensuring this function always
-# explores the lowest cost path as priority on each iteration 
+# explores the lowest cost path as priority on each iteration
 # and ultimately returns the lowest score possible to get to end of maze
 def walk_maze(grid, start_position, end_position):
     start_r, start_c = start_position
     end_r, end_c = end_position
+    viable_paths = defaultdict(list)
 
     queue = []
-    heapq.heappush(queue, (0, start_r, start_c, ">"))
-
+    heapq.heappush(queue, (0, start_r, start_c, ">", [(start_r, start_c)]))
     visited = set()
 
     while queue:
         # The head position of the lowest score path so far
-        score, r, c, direction = heapq.heappop(queue)
+        score, r, c, dir, path = heapq.heappop(queue)
 
-        # Skip if aleady visited
-        if (r, c, direction) in visited:
-            continue
-        visited.add((r, c, direction))
+        visited.add((r, c, dir))
 
         # Reached the end
         if (r, c) == (end_r, end_c):
-            return score
+            viable_paths[score].append(path)
 
         # Otherwise, explore neighbours
-        for nr, nc, ndir, move_cost in get_neighbours(grid, r, c, direction):
+        for nr, nc, ndir, move_cost in get_neighbours(grid, r, c, dir):
             n_score = score + move_cost
             if (nr, nc, ndir) not in visited:
-                heapq.heappush(queue, (n_score, nr, nc, ndir))
+                heapq.heappush(queue, (n_score, nr, nc, ndir, path + [(nr, nc)]))
 
-    return -1
+    return viable_paths
 
 
 # Main
 grid = read_file("day16.txt")
 
+# start and end
 start_r, start_c = len(grid) - 2, 1
 end_r, end_c = 1, len(grid[0]) - 2
 
-result = walk_maze(grid, (start_r, start_c), (end_r, end_c))
-print("part_1:", result)
+viable_paths = walk_maze(grid, (start_r, start_c), (end_r, end_c))
+
+lowest_cost = list(viable_paths.keys())[0]
+print("part_1:", lowest_cost)
+
+tiles_on_best_paths = set()
+for path in viable_paths[lowest_cost]:
+    tiles_on_best_paths.update(path)
+
+print("part_2:", len(tiles_on_best_paths))
