e26ae23d4d0e8a4821c146b9292e4657661dbeff
Vijay Patel <spiritedtechie@gmail.com>
2024-12-19 22:51:18 +0000

    day 17 - simplify / concise logic using python case statement

--- day17.py
+++ day17.py
@@ -14,68 +14,17 @@ def parse_file(filename):
     return registers, program
 
 
-combos = {
-    0: lambda r_a, r_b, r_c: 0,
-    1: lambda r_a, r_b, r_c: 1,
-    2: lambda r_a, r_b, r_c: 2,
-    3: lambda r_a, r_b, r_c: 3,
-    4: lambda r_a, r_b, r_c: r_a,
-    5: lambda r_a, r_b, r_c: r_b,
-    6: lambda r_a, r_b, r_c: r_c,
-}
+def compute(r_a, r_b, r_c, program):
+    combos = {
+        0: lambda: 0,
+        1: lambda: 1,
+        2: lambda: 2,
+        3: lambda: 3,
+        4: lambda: r_a,
+        5: lambda: r_b,
+        6: lambda: r_c,
+    }
 
-
-def op_0(r_a, r_b, r_c, operand):
-    r_a = r_a // (2 ** combos[operand](r_a, r_b, r_c))
-    return r_a, r_b, r_c
-
-
-def op_1(r_a, r_b, r_c, operand):
-    r_b = r_b ^ operand
-    return r_a, r_b, r_c
-
-
-def op_2(r_a, r_b, r_c, operand):
-    r_b = combos[operand](r_a, r_b, r_c) % 8
-    return r_a, r_b, r_c
-
-
-def op_3(r_a, r_b, r_c, operand):
-    return r_a != 0
-
-
-def op_4(r_a, r_b, r_c, operand):
-    r_b = r_b ^ r_c
-    return r_a, r_b, r_c
-
-
-def op_5(r_a, r_b, r_c, operand):
-    return combos[operand](r_a, r_b, r_c) % 8
-
-
-def op_6(r_a, r_b, r_c, operand):
-    r_b = r_a // 2 ** combos[operand](r_a, r_b, r_c)
-    return r_a, r_b, r_c
-
-
-def op_7(r_a, r_b, r_c, operand):
-    r_c = r_a // 2 ** combos[operand](r_a, r_b, r_c)
-    return r_a, r_b, r_c
-
-
-op_code_function = {
-    0: op_0,
-    1: op_1,
-    2: op_2,
-    3: op_3,
-    4: op_4,
-    5: op_5,
-    6: op_6,
-    7: op_7,
-}
-
-
-def execute(r_a, r_b, r_c, program):
     output = []
 
     pointer = 0
@@ -83,29 +32,52 @@ def execute(r_a, r_b, r_c, program):
         op_code = program[pointer]
         operand = program[pointer + 1]
 
-        func = op_code_function[op_code]
+        match op_code:
+            case 0:
+                r_a = r_a // (2 ** combos[operand]())
+            case 1:
+                r_b = r_b ^ operand
+            case 2:
+                r_b = combos[operand]() % 8
+            case 3:
+                pointer = operand - 2 if r_a != 0 else pointer
+            case 4:
+                r_b = r_b ^ r_c
+            case 5:
+                output.append(combos[operand]() % 8)
+            case 6:
+                r_b = r_a // 2 ** combos[operand]()
+            case 7:
+                r_c = r_a // 2 ** combos[operand]()
 
-        if op_code == 5:
-            output.append(func(r_a, r_b, r_c, operand))
-            pointer += 2
-        elif op_code == 3:
-            if func(r_a, r_b, r_c, operand):
-                pointer = operand
-            else:
-                pointer += 2
-        else:
-            r_a, r_b, r_c = func(r_a, r_b, r_c, operand)
-            pointer += 2
+        pointer += 2
 
     return r_a, r_b, r_c, output
 
 
 # Main
-registers, program = parse_file("day17_test.txt")
+registers, program = parse_file("day17.txt")
 
 # Part 1
 r_a = int(registers["A"])
 r_b = int(registers["B"])
 r_c = int(registers["C"])
-r_a, r_b, r_c, output = execute(r_a, r_b, r_c, program)
+r_a, r_b, r_c, output = compute(r_a, r_b, r_c, program)
 print("part 1:", output)
+
+
+# # Part 2
+# r_a = 43980465111040
+# r_b = 0
+# r_c = 0
+# while r_a != 0:
+#     _, _, _, output = execute(r_a, r_b, r_c, program)
+#     print(r_a, r_b, r_c, output)
+
+#     if output == program:
+#         print(r_a, output)
+
+#     import time
+#     time.sleep(0.5)
+
+#     r_a += 1
