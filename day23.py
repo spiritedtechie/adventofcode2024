c285890196e975a5150afe0ff0cd3b623286a0bd
Vijay Patel <spiritedtechie@gmail.com>
2024-12-23 20:48:08 +0000

    day 23 - refactor to separate finding connected groups and counting groups with t

--- day23.py
+++ day23.py
@@ -15,23 +15,17 @@ for line in network_map:
     graph[node2].add(node1)
 
 
-#  part 1
-groups_with_t = 0
-visited = set()
+# part 1
+groups = set()
 for n1 in graph:
     for n2 in graph[n1]:
         common_neighbors = graph[n1].intersection(graph[n2])
-
         for c in common_neighbors:
-            connected_group = frozenset([n1, n2, c])
+            connected_group = frozenset((n1, n2, c))
+            groups.add(connected_group)
 
-            # ignore essentially the same triple
-            if connected_group in visited:
-                continue
-            visited.add(connected_group)
 
-            if any(node.startswith("t") for node in connected_group):
-                groups_with_t += 1
+groups_with_t = sum(any(node.startswith("t") for node in group) for group in groups)
 
 
 print("part 1:", groups_with_t)
