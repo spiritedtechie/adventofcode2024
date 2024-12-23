67ddaab632b5b36cff92cafb262f5d3ba032e6ff
Vijay Patel <spiritedtechie@gmail.com>
2024-12-23 18:15:48 +0000

    day 22 - refactor out sequence_price function

--- day22.py
+++ day22.py
@@ -13,11 +13,7 @@ def compute_next_secret(secret):
 
 
 def generate_secrets(secret, iterations=2000):
-    secrets = [secret]
-    for _ in range(iterations):
-        secret = compute_next_secret(secret)
-        secrets.append(secret)
-    return secrets
+    return [secret] + [secret := compute_next_secret(secret) for _ in range(iterations)]
 
 
 def get_prices(secrets):
@@ -26,24 +22,29 @@ def get_prices(secrets):
     return prices, price_diffs
 
 
+def sequence_price(prices, price_diffs, seq_len=4):
+    seq_prices = []
+    visited_seqs = set()
+    for seq_start in range(len(price_diffs) - seq_len + 1):
+        seq = tuple(price_diffs[seq_start : seq_start + seq_len])
+        if seq not in visited_seqs:
+            seq_prices.append((seq, prices[seq_start + seq_len]))
+            visited_seqs.add(seq)
+    return seq_prices
+
+
 def part_1(numbers):
     return sum(secrets[-1] for secrets in (generate_secrets(num) for num in numbers))
 
 
-def part2(numbers, seq_len=4):
+def part2(numbers):
     seq_banana_counts = Counter()
 
     for num in numbers:
         secrets = generate_secrets(num)
         prices, price_diffs = get_prices(secrets)
-
-        visited_seqs = set()
-        for seq_start in range(len(price_diffs) - seq_len + 1):
-            seq = tuple(price_diffs[seq_start : seq_start + seq_len])
-
-            if seq not in visited_seqs:
-                seq_banana_counts[seq] += prices[seq_start + seq_len]
-                visited_seqs.add(seq)
+        for seq, price in sequence_price(prices, price_diffs):
+            seq_banana_counts[seq] += price
 
     most_common_seq = seq_banana_counts.most_common(1)
     return most_common_seq[0][1] if most_common_seq else 0
